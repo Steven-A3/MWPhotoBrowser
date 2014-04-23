29d084b682d42bbebf5dfaf97c9977349d0ed8ef
AllAboutApps <cs@allaboutapps.net>
2014-04-23 18:58:25 +0900

    Share button 눌렀을 때 프로그레스바 나타나지 않도록 수정

--- MWPhotoBrowser/Classes/MWPhotoBrowser.m
+++ MWPhotoBrowser/Classes/MWPhotoBrowser.m
@@ -1467,21 +1467,21 @@
                     }
                     self.activityViewController = [[UIActivityViewController alloc] initWithActivityItems:items applicationActivities:nil];
                     
-                    // Show loading spinner after a couple of seconds
-                    double delayInSeconds = 2.0;
-                    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
-                    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
-                        if (self.activityViewController) {
-                            [self showProgressHUDWithMessage:nil];
-                        }
-                    });
+//                    // Show loading spinner after a couple of seconds
+//                    double delayInSeconds = 2.0;
+//                    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+//                    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+//                        if (self.activityViewController) {
+//                            [self showProgressHUDWithMessage:nil];
+//                        }
+//                    });
 
                     // Show
                     typeof(self) __weak weakSelf = self;
                     [self.activityViewController setCompletionHandler:^(NSString *activityType, BOOL completed) {
                         weakSelf.activityViewController = nil;
                         [weakSelf hideControlsAfterDelay];
-                        [weakSelf hideProgressHUD:YES];
+//                        [weakSelf hideProgressHUD:YES];
                     }];
                     [self presentViewController:self.activityViewController animated:YES completion:nil];
                     
